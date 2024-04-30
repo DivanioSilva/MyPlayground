507a9a6a888815140d32b6741a786b6a9a86726a
Divanio Silva <dcsilva@mail.ru>
2024-04-30 16:02:08 +0100

    Adding the Keycloak module and refactoring the code

--- Keycloak/src/main/java/com/ds/keycloak/mapper/PersonMapper.java
+++ Keycloak/src/main/java/com/ds/keycloak/mapper/PersonMapper.java
@@ -4,13 +4,11 @@ import com.ds.keycloak.domain.Person;
 import com.ds.keycloak.dto.PersonDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PersonMapper {
-    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
     PersonDto toDto(Person person);
     Person toPerson(PersonDto personDto);
