d4d323e6c52b79a6f4e69f96f65540262a49de0a
Divanio Silva <dcsilva@mail.ru>
2025-01-05 09:50:35 +0000

    Keycloak users importer module creation

---
+++ Keycloak-users-importer/src/main/java/com/ds/keycloakusersimporter/KeycloakUsersImporterApplication.java
@@ -0,0 +1,13 @@
+package com.ds.keycloakusersimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakUsersImporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakUsersImporterApplication.class, args);
+    }
+
+}
---
+++ Keycloak-users-importer/src/test/java/com/ds/keycloakusersimporter/KeycloakUsersImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.keycloakusersimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakUsersImporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
